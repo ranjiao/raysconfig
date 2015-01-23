909180222365a13e47b8952e544439b0e138f60c
Ran Jiao <ranjiao@bytedance.com>
2015-01-23 16:05:55 +0800

    key bindings for helm

--- myemacs/config/init_keys.el
+++ myemacs/config/init_keys.el
@@ -162,6 +162,9 @@
 (global-set-key (kbd "C-c <") 'python-shift-left)
 
 ;; helm
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-mini)
 (setq helm-buffers-fuzzy-matching t
